cfd1273b975da02302eb2b97463fb498fc6086b6
Steven Lee <stevenyslee@gmail.com>
2018-08-27 12:03:53 -0700

    Create dropdown menu, radio button, and generate button

--- client/src/components/Topshelf.jsx
+++ client/src/components/Topshelf.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import { Button, ButtonGroup, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import SplitMenu from './SplitMenu.jsx';
 import LengthMenu from './LengthMenu.jsx';
 import GeneratePlan from './GeneratePlan.jsx';
 
 const Topshelf = (props) => (
   <div id='Topshelf'>
-    <SplitMenu />
-    <LengthMenu />
-    <GeneratePlan />
+  <Dropdown isOpen={props.state.splitDropdownOpen} toggle={props.toggleSplit}>
+  <DropdownToggle caret color="primary">
+    Split
+  </DropdownToggle>
+  <DropdownMenu>
+    <DropdownItem>Chest</DropdownItem>
+    <DropdownItem divider/>
+    <DropdownItem>Shoulders</DropdownItem>
+    <DropdownItem divider/>
+    <DropdownItem>Back</DropdownItem>
+    <DropdownItem divider/>
+    <DropdownItem>Legs</DropdownItem>
+    <DropdownItem divider/>
+    <DropdownItem>Arms</DropdownItem>
+    <DropdownItem divider/>
+    <DropdownItem>Abs</DropdownItem>
+    <DropdownItem divider/>
+    <DropdownItem>Fullbody</DropdownItem>
+  </DropdownMenu>
+  </Dropdown>
+
+  <ButtonGroup>
+    <Button color="primary" onClick={() => props.onRadioBtnClick(1)} active={props.state.rSelected === 1}>One</Button>
+    <Button color="primary" onClick={() => props.onRadioBtnClick(2)} active={props.state.rSelected === 2}>Two</Button>
+    <Button color="primary" onClick={() => props.onRadioBtnClick(3)} active={props.state.rSelected === 3}>Three</Button>
+  </ButtonGroup>
+
+  <Button color="primary" size="lg" active>Generate Workout</Button>{' '}
   </div>
 )
 
