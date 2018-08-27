0e434e9d745a031ab02dbfef8d10d8002a4a3fb6
Steven Lee <stevenyslee@gmail.com>
2018-08-27 12:04:16 -0700

    Toggle buttons

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,29 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Topshelf from './components/Topshelf.jsx';
 import Workout from './components/Workout.jsx';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		// this.state  {
-			
-		// }
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      splitDropdownOpen: false
+    };
 
-	componentDidMount() {
-		console.log('Component Mounted');
-	}
+    this.toggleSplit = this.toggleSplit.bind(this);
+    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
+  }
 
-	render() {
-		return (
-		<div>
-			<Topshelf />
-			<Workout />
-		</div>
-		)
-	}
+  toggleSplit() {
+    this.setState(prevState => ({
+      splitDropdownOpen: !prevState.splitDropdownOpen
+    }));
+  }
+
+  onRadioBtnClick(rSelected) {
+    this.setState({ rSelected });
+    // asynchronous console logging
+    console.log(this.state.rSelected);
+
+  }
+
+  componentDidMount() {
+    console.log('Component Mounted');
+  }
+
+  render() {
+    return (
+    <div>
+      <Topshelf
+        toggleSplit={this.toggleSplit}
+        onRadioBtnClick={this.onRadioBtnClick}
+        state={this.state} 
+      />
+      <Workout />
+    </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
