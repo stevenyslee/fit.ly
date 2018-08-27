16a9df3eac1d582cfe702eb3049763fbb220eb8d
Steven Lee <stevenyslee@gmail.com>
2018-08-27 15:23:46 -0700

    Serve endpoints

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../db/');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/:split/:length', (req, res) => {
+	const { params } = req;
+	console.log(params);
+  db.getWorkout(params.split, params.length, (results) => (res.send(results)));
+});
 
 app.listen(3000, () => console.log('listening on port 3000'));
