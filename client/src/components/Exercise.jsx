0a07aa01fb52cf7944d7659c73790b00a6b676d3
Steven Lee <stevenyslee@gmail.com>
2018-08-27 16:12:12 -0700

    Embed Youtube videos

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const Exercise = (props) => (
   <div>
     <h4>{props.exercise.exercise}</h4>
+    <img src={props.exercise.picture} alt={props.exercise.exercise} height="42" width="42" />
+    <iframe width="560" height="315" src={props.exercise.video} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
   </div>
 )
 
